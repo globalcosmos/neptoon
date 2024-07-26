aa7b16223701427f60409072d445ec7e5eeaba46
Daniel Power <daniel.power@ufz.de>
2024-07-26 17:09:49 +0200

    -outline of features

---
+++ neptoon/quality_assesment/smoothing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+""" 
+Features:
+    - Class for smoothing
+    - neutrons and SM need "FINAL" ColumnInfo.Name variables
+    - update feature to ensure code works on most current version of
+      variable
+    - method to ensure ColumnInfo is managed correctly
+ """
