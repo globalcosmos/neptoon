e3c1b7d62837bb7163805b0454b9d3b12cfd9f68
Martin Schrön <martin.schroen@ufz.de>
2024-08-23 17:30:36 +0200

    New configuration page

--- Neptoon-gui.py
+++ Neptoon-gui.py
@@ -12,10 +12,21 @@ st.logo(
     # icon_image=LOGO_URL_SMALL,
 )
 
+# Initialize session state
+if 'config_name' not in st.session_state:
+    st.session_state.config_name = 'Sensor_A42'
+if 'config_changed' not in st.session_state:
+    st.session_state.config_changed = False
+
 pages = [
+    # st.Page(
+    #     "GUI/Welcome.py",
+    #     title="Welcome"
+    # ),
     st.Page(
-        "GUI/Welcome.py",
-        title="Welcome"
+        "GUI/Configuration.py",
+        title=st.session_state.config_name,
+        icon=":material/warning:" if st.session_state.config_changed else ":material/check:"
     ),
     st.Page(
         "GUI/Calibration.py",
