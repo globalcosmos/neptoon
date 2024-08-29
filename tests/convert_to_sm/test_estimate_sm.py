2f1a2d837d46a38e53f6d31c45b9beb469cf898e
Daniel Power <daniel.power@ufz.de>
2024-08-29 17:36:23 +0200

    WIP - tests - more work needed

--- tests/convert_to_sm/test_estimate_sm.py
+++ tests/convert_to_sm/test_estimate_sm.py
@@ -84,25 +84,27 @@ def test_convert_soc_to_wsom():
 
 def test_calculate_sm_estimates(neutrons_to_sm_instance):
     """Test the calculate_sm_estimates method."""
-    neutrons_to_sm_instance.calculate_sm_estimates()
+    neutrons_to_sm_instance.calculate_sm_estimates(
+        neutron_data_column_name=str(
+            ColumnInfo.Name.CORRECTED_EPI_NEUTRON_COUNT_FINAL
+        ),
+        soil_moisture_column_write_name=str(ColumnInfo.Name.SOIL_MOISTURE),
+    )
     assert (
         str(ColumnInfo.Name.SOIL_MOISTURE)
         in neutrons_to_sm_instance.crns_data_frame.columns
     )
 
 
-def test_process_data(neutrons_to_sm_instance):
-    """Test the process_data method."""
-    neutrons_to_sm_instance.process_data()
-    assert (
-        str(ColumnInfo.Name.CORRECTED_EPI_NEUTRON_COUNT_FINAL)
-        in neutrons_to_sm_instance.crns_data_frame.columns
-    )
-    assert (
-        str(ColumnInfo.Name.SOIL_MOISTURE)
-        in neutrons_to_sm_instance.crns_data_frame.columns
-    )
-    assert (
-        str(ColumnInfo.Name.SOIL_MOISTURE_MEASURMENT_DEPTH)
-        in neutrons_to_sm_instance.crns_data_frame.columns
-    )
+# def test_process_data(neutrons_to_sm_instance):
+#     """Test the process_data method."""
+#     neutrons_to_sm_instance.calculate_all_soil_moisture_data()
+
+#     assert (
+#         str(ColumnInfo.Name.SOIL_MOISTURE)
+#         in neutrons_to_sm_instance.crns_data_frame.columns
+#     )
+#     assert (
+#         str(ColumnInfo.Name.SOIL_MOISTURE_MEASURMENT_DEPTH)
+#         in neutrons_to_sm_instance.crns_data_frame.columns
+#     )
