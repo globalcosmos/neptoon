b501bae446efc547b5738a5418c282d9ce530a5d
Daniel Power <daniel.power@ufz.de>
2024-09-06 14:39:10 +0200

    update tests

--- tests/data_management/test_save_data.py
+++ tests/data_management/test_save_data.py
@@ -1,27 +1,11 @@
 import pandas as pd
 import pytest
+import math
 from pathlib import Path
 from unittest.mock import Mock, patch
 from neptoon.data_management.save_data import SaveAndArchiveOutputs
 from neptoon.data_management.site_information import SiteInformation
 from neptoon.data_management.data_audit import DataAuditLog, log_key_step
-
-
-# # @pytest.fixture
-# def sample_crns_data():
-#     return pd.DataFrame(
-#         {
-#             "date_time": pd.date_range(
-#                 start="2023-01-01", periods=5, freq="h"
-#             ),
-#             "epithermal_neutrons_raw": [100, 110, 105, 115, 108],
-#             "epithermal_neutrons_cph": [100, 110, 105, 115, 108],
-#             "air_pressure": [1000, 1005, 1002, 998, 1001],
-#             "air_relative_humidity": [80, 75, 76, 65, 89],
-#             "air_temperature": [23, 24, 25, 23, 20],
-#         }
-#     ).set_index("date_time")
-
 
 UNFLAGGED = "UNFLAGGED"
 BAD = "BAD"
@@ -124,7 +108,6 @@ def test_create_save_folder(save_and_archive, tmp_path):
     """
     Tests that the save folder is correctly created
     """
-    save_and_archive.site_information.site_name = "TestSite2"
     save_and_archive.create_save_folder()
     expected_path = tmp_path / "test_folder"
     assert expected_path.exists()
@@ -142,6 +125,9 @@ def test_create_save_folder_existing(save_and_archive, tmp_path):
 
 
 def test_append_hash_to_folder_name(save_and_archive, tmp_path):
+    """
+    Test appending the hash to the folder.
+    """
     folder_path = tmp_path / "test_folder"
     folder_path.mkdir()
     (folder_path / "data_audit_log").mkdir()
@@ -154,88 +140,12 @@ def test_append_hash_to_folder_name(save_and_archive, tmp_path):
     assert not folder_path.exists()
 
 
-# def sample_flags_data():
-#     return pd.DataFrame(
-#         {
-#             "date_time": pd.date_range(
-#                 start="2023-01-01", periods=5, freq="h"
-#             ),
-#             "epithermal_neutrons_raw": [
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "BAD",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#             ],
-#             "epithermal_neutrons_cph": [
-#                 "UNFLAGGED",
-#                 "BAD",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#             ],
-#             "air_pressure": [
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "BAD",
-#                 "UNFLAGGED",
-#             ],
-#             "air_relative_humidity": [
-#                 "BAD",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#             ],
-#             "air_temperature": [
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "UNFLAGGED",
-#                 "BAD",
-#             ],
-#         }
-#     ).set_index("date_time")
-
-
-# def sample_site_information():
-#     site_information = SiteInformation(
-#         site_name="Test_Site",
-#         latitude=51.37,
-#         longitude=12.55,
-#         elevation=140,
-#         reference_incoming_neutron_value=150,
-#         dry_soil_bulk_density=1.4,
-#         lattice_water=0.01,
-#         soil_organic_carbon=0,
-#         n0=700,
-#         cutoff_rigidity=2.94,
-#         site_biomass=1,
-#     )
-#     return site_information
-
-
-# df = sample_crns_data()
-# flag_df = sample_flags_data()
-# site_info = sample_site_information()
-
-# DataAuditLog.create()
-
-
-# @log_key_step("f")
-# def dummyfunc(f=1):
-#     return f
-
-
-# dummyfunc()
-
-
-# test_saver = SaveAndArchiveOutputs(
-#     "process_test_site",
-#     processed_data_frame=df,
-#     flag_data_frame=flag_df,
-#     site_information=site_info,
-#     append_yaml_hash_to_folder_name=True,
-# )
-# test_saver.save_outputs()
+def test_mask_bad_data(save_and_archive):
+    """
+    Test data masking
+    """
+    masked_df = save_and_archive.mask_bad_data()
+    assert pd.isna(masked_df["epithermal_neutrons_raw"][3])
+    assert pd.isna(masked_df["epithermal_neutrons_cph"][1])
+    assert pd.isna(masked_df["epithermal_neutrons_cph"][2])
+    assert masked_df["epithermal_neutrons_raw"][0] == 100
