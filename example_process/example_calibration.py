1943eda9271a40e83f9bb16e2021f92f3a7031b3
Daniel Power <daniel.power@ufz.de>
2024-09-22 14:37:32 +0200

    -add example calibration to example process folder

---
+++ example_process/example_calibration.py
@@ -0,0 +1,47 @@
+# %%
+import pandas
+from pathlib import Path
+from neptoon.calibration.station_calibration import (
+    CalibrationConfiguration,
+    CalibrationStation,
+)
+
+# %%
+
+calibration_data = pandas.read_csv(
+    (
+        Path.cwd().parent
+        / Path("tests/calibration/mock_data/Sheepdrove2-calibration.csv")
+    ),
+    skipinitialspace=True,
+)
+crns_data = pandas.read_csv(
+    Path.cwd().parent
+    / Path("tests/calibration/mock_data/Sheepdrove2-CRNS.csv"),
+    index_col=0,
+    parse_dates=True,
+)
+
+calib_config = CalibrationConfiguration(
+    date_time_column_name="DateTime_utc",
+    distance_column="Distance_to_CRNS_m",
+    sample_depth_column="Profile_Depth_cm",
+    bulk_density_of_sample_column="DryBulkDensity_g_cm3",
+    soil_moisture_gravimetric_column="SoilMoisture_g_g",
+    soil_organic_carbon_column="SoilOrganicCarbon_g_g",
+    lattice_water_column="LatticeWater_g_g",
+    profile_id_column="Profile_ID",
+    air_humidity_column_name="AirHumidity_gapfilled",
+    neutron_column_name="NeutronCount_Epithermal_MovAvg24h_corrected",
+)
+# %%
+
+calibration_station = CalibrationStation(
+    calibration_data=calibration_data,
+    time_series_data=crns_data,
+    config=calib_config,
+)
+estimate_n0 = calibration_station.find_n0_value()
+print(f"N0 number is {estimate_n0}")
+calibration_station.return_calibration_results_data_frame()
+# %%
