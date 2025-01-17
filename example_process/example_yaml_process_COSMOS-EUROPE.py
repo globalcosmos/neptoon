343538d7f69b3e8278b03d26dcc86452c335098d
Daniel Power <daniel.power@ufz.de>
2025-01-17 10:14:58 +0100

    - quick fix

--- example_process/example_yaml_process_COSMOS-EUROPE.py
+++ example_process/example_yaml_process_COSMOS-EUROPE.py
@@ -33,8 +33,8 @@ yaml_processor = ProcessWithYaml(configuration_object=config)
 yaml_processor.run_full_process()
 
 
-# Magazine.topics
-# with Publish("Report-FSC001_station.pdf", "FSC001 data") as M:
-#     for topic in Magazine.topics:
-#         M.add_topic(topic)
-#         M.add_figure(topic)
+Magazine.topics
+with Publish("Report-FSC001_station.pdf", "FSC001 data") as M:
+    for topic in Magazine.topics:
+        M.add_topic(topic)
+        M.add_figure(topic)
