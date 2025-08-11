391de58c2351b2b64ddc5e192428e4251c5780b5
Daniel Power <daniel.power@ufz.de>
2025-08-11 11:33:48 +0200

    fix by patching saqc modules

--- neptoon/__init__.py
+++ neptoon/__init__.py
@@ -1,6 +1,17 @@
+import sys
+from types import ModuleType
+
+# Create fake module with the classes SaQC
+fake_gui = ModuleType("saqc.lib.selectionGUI")
+fake_gui.MplScroller = type("MplScroller", (), {})
+fake_gui.SelectionOverlay = type("SelectionOverlay", (), {})
+
+sys.modules["saqc.lib.selectionGUI"] = fake_gui
+
+
 from .hub import CRNSDataHub
 
 
-VERSION = 'v0.11.2'
+VERSION = "v0.11.2"
 
 __version__ = VERSION
